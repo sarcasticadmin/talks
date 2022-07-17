ef759462130406dcda0676161d1bf22d0bfba62e
Robert James Hernandez <rob@sarcasticadmin.com>
2022-07-17 15:54:27 -0700

    Scale 19x openwrt talk slides

--- openwrt-at-scale/default.nix
+++ openwrt-at-scale/default.nix
@@ -1,12 +1,15 @@
 let
   pkgs = import <nixpkgs> {};
 
+  # https://ctan.org/ for what to combine in texlive
   texlive = pkgs.texlive.combine {
     inherit (pkgs.texlive)
     scheme-small
     beamer
+    # Theme reqs
     beamertheme-metropolis
     pgfopts
+    multirow
     ;
   };
 in {
